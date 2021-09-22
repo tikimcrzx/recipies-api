9fbfe02bb90d1e4b476ab24c948bc883c4f447dc
tikimcrzx <tikimioo723@gmail.com>
2021-09-21 21:24:17 -0700

    new recipe endpoint

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
 
 var recipes []Recipe
@@ -22,8 +23,18 @@ func NewRecipeHandler(c *gin.Context) {
 	var recipe Recipe
 	if err := c.ShouldBindJSON(&recipe); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
+			"error": err.Error()})
 		return
 	}
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
+}
+
+func main() {
+	recipes = make([]Recipe, 0)
+	router := gin.Default()
+	router.POST("/recipes", NewRecipeHandler)
+	router.Run()
 }
